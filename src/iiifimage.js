4c028e9fd69edc7f8681f529706186d1cefae149
Chris Beer <cabeer@stanford.edu>
2019-03-16 16:07:04 -0700

    Initial commit

---
+++ src/iiifimage.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react'
+import axios from 'axios';
+import URLImage from './urlimage';
+
+export default class IIIFImage extends Component {
+  static intersects(a, b) {
+    return (a.x1 <= b.x2 &&
+            b.x1 <= a.x2 &&
+            a.y1 <= b.y2 &&
+            b.y1 <= a.y2);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { data: undefined };
+    this.fetchImageApiResponse();
+  }
+
+  fetchImageApiResponse() {
+    const { url } = this.props;
+    axios.get(`${url}/info.json`).then((response) => {
+      this.setState({ data: response.data });
+    });
+  }
+
+  tiles() {
+    const { url, scale, zoom, bounds } = this.props;
+    const { data } = this.state;
+
+    if (!data) return [];
+
+    const result = [];
+
+    const { width, height, tiles } = data;
+    const { width: tileWidth, height: tileHeight, scaleFactors } = data.tiles[0];
+
+    result.push({
+      key: 'base',
+      src: `${url}/full/400,/0/default.jpg`,
+      x: 0,
+      y: 0,
+      width: width,
+      height: height,
+    });
+
+    const tileZoom = 1 / ((scaleFactors || []).sort(function(a,b) { return a < b }).find((f) => { return 1/f > scale }) || 1);
+
+    for (var i = 0; i < Math.ceil(tileZoom * width / tileWidth); i++) {
+      for (var j = 0; j < Math.ceil(tileZoom * height / tileHeight); j++) {
+
+        let actualTileWidth = tileWidth, actualTileHeight = tileHeight;
+
+        // last in the row
+        if (i === Math.floor(tileZoom * width / tileWidth)) {
+          actualTileWidth = Math.ceil(tileZoom * width) - i * tileWidth;
+
+        }
+
+        // last in the column
+        if (j === Math.floor(tileZoom * height / tileHeight)) {
+          actualTileHeight = Math.ceil(tileZoom * height) - j * tileHeight;
+        }
+
+        const x = Math.floor(i * tileWidth / tileZoom),
+              y = Math.floor(j * tileHeight / tileZoom),
+              x_width = Math.ceil(actualTileWidth / tileZoom),
+              y_height = Math.ceil(actualTileHeight / tileZoom)
+
+        const tileBounds = { x1: x, y1: y, x2: x + x_width, y2: y + y_height };
+
+        if (IIIFImage.intersects(bounds, tileBounds)) {
+          result.push({
+            key: `${x},${y}`,
+            src: `${url}/${x},${y},${x_width},${y_height}/${actualTileWidth},${actualTileHeight}/0/default.jpg`,
+            x: x,
+            y: y,
+            width: x_width,
+            height: y_height,
+          });
+        }
+      }
+    }
+
+    return result;
+  }
+
+  fitBounds({ width, height, maxWidth, maxHeight }) {
+    const aspectRatio = width / height;
+
+    if ((maxWidth / maxHeight) < aspectRatio) {
+      return { height: maxWidth / aspectRatio, width: maxWidth };
+    } else {
+      return { height: maxHeight, width: maxHeight * aspectRatio };
+    }
+
+  }
+
+  render() {
+    return (
+      this.tiles().map((tile) => {
+        return <URLImage {...tile} />;
+      })
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,84 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
+import { Stage, Layer, Rect, Image } from 'react-konva';
+import debounce from 'lodash/debounce';
+import IIIFImage from './iiifimage';
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: props.zoom || 1,
+      x: props.x || 0,
+      y: props.y || 0,
+      scale: 0.1,
+    };
+  }
+
+  handleWheel = (e) => {
+    const { zoom } = this.state;
+
+    e.evt.preventDefault();
+    const stage = e.currentTarget;
+
+    var oldScale = stage.scaleX();
+    var scaleBy = 1.03;
+
+    var mousePointTo = {
+      x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+    };
+
+    var newScale =
+      e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    stage.scale({ x: newScale, y: newScale });
+
+    var newPos = {
+      x:
+        -(mousePointTo.x - stage.getPointerPosition().x / newScale) *
+        newScale,
+      y:
+        -(mousePointTo.y - stage.getPointerPosition().y / newScale) *
+        newScale
+    };
+
+    stage.position(newPos);
+    stage.batchDraw();
+
+    this.asyncSetState({
+      ...newPos,
+      zoom: newScale,
+      scale: newScale,
+    });
+  }
+
+  asyncSetState = debounce((props) => {
+    this.setState(props);
+  }, 500);
+
+  handleDragEnd = e => {
+    this.setState({
+      x: e.target.x(),
+      y: e.target.y(),
+    });
+  };
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    const { x, y, scale, zoom } = this.state;
+
+    return (
+      <Fragment>
+        <Stage x={x} y={y} scaleX={scale} scaleY={scale} onWheel={this.handleWheel} onDragEnd={this.handleDragEnd} draggable width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <IIIFImage zoom={zoom} scale={scale} bounds={{ x1: -1*x / scale, y1: -1*y / scale, x2: -1*x / scale + (window.innerWidth / scale), y2: -1*y / scale + (window.innerHeight / scale) }} url="https://stacks.stanford.edu/image/iiif/bb000zn0114%252FPC0062_2008-194_Q03_02_007" />
+          </Layer>
+        </Stage>
+        <div style={{ position: 'absolute', right: 0, top: 0 }} >
+          <button onClick={() => { this.setState({ scale: scale * 2 })} }>zoom in</button>
+          <button onClick={() => { this.setState({ scale: scale / 2 })} }>zoom out</button>
+          <button onClick={() => { this.setState({ scale: 1 })} }>reset</button>
+        </div>
+      </Fragment>
+    );
   }
 }
