315237cf542d712fbf28f1255216d407a0b48214
Chris Beer <cabeer@stanford.edu>
2019-03-18 14:27:50 -0700

    Add a demo control for changing the IIIF url

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,9 +4,24 @@ import {render} from 'react-dom'
 import Example from '../../src'
 
 class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: "https://stacks.stanford.edu/image/iiif/bb000zn0114%252FPC0062_2008-194_Q03_02_007" };
+  }
+
+  handleChange = (e) => {
+    this.setState({ url: e.target.value });
+  }
+
   render() {
+    const { url } = this.state;
     return <div>
-      <Example url="https://stacks.stanford.edu/image/iiif/bb000zn0114%252FPC0062_2008-194_Q03_02_007" />
+      <label>
+        IIIF Image URL:
+        <input value={url} onChange={this.handleChange} />
+      </label>
+
+      <Example url={url} />
     </div>
   }
 }
